bf056fba4885e71c8f4844eda7697d9a4e057769
Dawid Jarosz <jarosz199806@gmail.com>
2026-01-02 16:21:20 +0100

    Add service constructor and method for fetching generation data

--- EnergyMixApi/EnergyMixApi/Services/CarbonIntensityService.cs
+++ EnergyMixApi/EnergyMixApi/Services/CarbonIntensityService.cs
@@ -4,6 +4,18 @@ namespace EnergyMixApi.Services
 {
     public class CarbonIntensityService : ICarbonIntensityService
     {
+        private readonly HttpClient _httpClient;
+
+        /// <summary>
+        /// Initializes the service and sets base Url address
+        /// </summary>
+        /// <param name="httpClient"></param>
+        public CarbonIntensityService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri("https://api.carbonintensity.org.uk");
+        }
+
         public async Task<EnergyMixResponse> GetEnergyMixInfo()
         {
             throw new NotImplementedException();
@@ -12,6 +24,20 @@ namespace EnergyMixApi.Services
         public async Task<OptimalWindowResponse> GetOptimalWindow(int hours)
         {
             throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Fetches generation mix data for the specified date range from the Carbon Intensity API
+        /// </summary>
+        /// <param name="from">The start date of the range for which to retrieve generation data</param>
+        /// <param name="to">The end date of the range for which to retrieve generation data</param>
+        private async Task<CarbonIntensityApiResponse> FetchGenerationData(DateTime from, DateTime to)
+        {
+            var url = $"/generation/{from:yyyy-MM-dd}T00:30Z/{to:yyyy-MM-dd}T00:00Z/";
+
+            var response = await _httpClient.GetFromJsonAsync<CarbonIntensityApiResponse>(url);
+
+            return response;
         }
     }
 }
