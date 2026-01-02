68c3ed06e0aca9edefa1a9448b32116aff77c698
Dawid Jarosz <jarosz199806@gmail.com>
2026-01-02 16:37:36 +0100

    Implement GetEnergyMixInfo with daily averages and clean energy share per day

--- EnergyMixApi/EnergyMixApi/Services/CarbonIntensityService.cs
+++ EnergyMixApi/EnergyMixApi/Services/CarbonIntensityService.cs
@@ -18,7 +18,55 @@ namespace EnergyMixApi.Services
 
         public async Task<EnergyMixResponse> GetEnergyMixInfo()
         {
-            throw new NotImplementedException();
+            var from = DateTime.UtcNow.Date;
+            var to = from.AddDays(3);
+
+            var data = await FetchGenerationData(from, to);
+
+            var groupedByDay = data.Data.GroupBy(d => d.From.Date);
+
+            var dailyDataList = new List<DailyEnergyData>();
+
+            foreach (var day in groupedByDay)
+            {
+                var energySource = day
+                    .SelectMany(interval => interval.GenerationMix)
+                    .Select(fuel => fuel.Fuel)
+                    .Distinct();
+
+                var sources = new Dictionary<string, double>();
+
+                foreach (var fuelName in energySource)
+                {
+                    var average = day
+                        .SelectMany(interval => interval.GenerationMix)
+                        .Where(fuel => fuel.Fuel == fuelName)
+                        .Average(fuel => fuel.Perc);
+
+                    sources.Add(fuelName, average);
+                }
+
+                var cleanEnergyPercent =
+                    sources["biomass"] +
+                    sources["nuclear"] +
+                    sources["hydro"] +
+                    sources["wind"] +
+                    sources["solar"];
+
+                var dailyEnergyData = new DailyEnergyData
+                {
+                    Date = day.Key,
+                    Sources = sources,
+                    CleanEnergyPercent = cleanEnergyPercent
+                };
+
+                dailyDataList.Add(dailyEnergyData);
+            }
+
+            return new EnergyMixResponse
+                    {
+                        Days = dailyDataList
+                    };
         }
 
         public async Task<OptimalWindowResponse> GetOptimalWindow(int hours)
@@ -33,6 +81,7 @@ namespace EnergyMixApi.Services
         /// <param name="to">The end date of the range for which to retrieve generation data</param>
         private async Task<CarbonIntensityApiResponse> FetchGenerationData(DateTime from, DateTime to)
         {
+            // starting at 00:30 to avoid getting last interval (23:30 - 00:00) from previous day 
             var url = $"/generation/{from:yyyy-MM-dd}T00:30Z/{to:yyyy-MM-dd}T00:00Z/";
 
             var response = await _httpClient.GetFromJsonAsync<CarbonIntensityApiResponse>(url);
