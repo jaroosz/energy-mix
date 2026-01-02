5433f2f5096a3b549c34a83f34f918acfc91a299
Dawid Jarosz <jarosz199806@gmail.com>
2026-01-02 14:16:23 +0100

    Add  JSON property mapping

--- EnergyMixApi/EnergyMixApi/Models/ApiModels.cs
+++ EnergyMixApi/EnergyMixApi/Models/ApiModels.cs
@@ -9,14 +9,19 @@ namespace EnergyMixApi.Models
 
     public class GenerationData
     {
+        [JsonPropertyName("from")]
         public DateTime From { get; set; }
+        [JsonPropertyName("to")]
         public DateTime To { get; set; }
+        [JsonPropertyName("generationmix")]
         public List<FuelSource> GenerationMix { get; set; }
     }
 
     public class FuelSource
     {
+        [JsonPropertyName("fuel")]
         public string Fuel { get; set; }
+        [JsonPropertyName("perc")]
         public double Perc { get; set; }
     }
 }
