fcb95f00d49e01979b7bbd552035dd2b946523ba
Dawid Jarosz <jarosz199806@gmail.com>
2026-01-02 14:12:22 +0100

    Implement Api Models

--- EnergyMixApi/EnergyMixApi/Models/ApiModels.cs
+++ EnergyMixApi/EnergyMixApi/Models/ApiModels.cs
@@ -1,6 +1,22 @@
-﻿namespace EnergyMixApi.Models
+﻿using System.Text.Json.Serialization;
+
+namespace EnergyMixApi.Models
 {
-    public class ApiModels
+    public class CarbonIntensityApiResponse
     {
+        public List<GenerationData> Data { get; set; }
+    }
+
+    public class GenerationData
+    {
+        public DateTime From { get; set; }
+        public DateTime To { get; set; }
+        public List<FuelSource> GenerationMix { get; set; }
+    }
+
+    public class FuelSource
+    {
+        public string Fuel { get; set; }
+        public double Perc { get; set; }
     }
 }
