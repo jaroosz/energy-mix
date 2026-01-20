06d957b7940ac6ee28ffa2f61636142eee1ea077
Dawid Jarosz <jarosz199806@gmail.com>
2026-01-20 17:54:10 +0100

    Add smoke tests for layout, chart card validation and optimal window functionality.

--- EnergyMixApi/EnergyMixApi.E2ETests/SeleniumUnitTest.cs
+++ EnergyMixApi/EnergyMixApi.E2ETests/SeleniumUnitTest.cs
@@ -18,5 +18,92 @@ namespace EnergyMinApi.E2ETests
             // Assert
             Assert.Contains("React App", driver.Title);
         }
+
+        [Fact]
+        public void Page_ShouldContainHeader_WhenHomePageIsOpened()
+        {
+            // Arrange
+            using IWebDriver driver = new EdgeDriver();
+
+            // Act
+            driver.Navigate().GoToUrl("https://energy-mix-frontend-53hj.onrender.com");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+
+            // Assert
+            Assert.True(driver.FindElement(By.TagName("header")).Displayed);
+        }
+
+        [Fact]
+        public void Page_ShouldContainMainContent_WhenHomePageIsOpened()
+        {
+            // Arrange
+            using IWebDriver driver = new EdgeDriver();
+
+            // Act
+            driver.Navigate().GoToUrl("https://energy-mix-frontend-53hj.onrender.com");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+
+            // Assert
+            Assert.True(driver.FindElement(By.ClassName("main-content-container")).Displayed);
+        }
+
+        [Fact]
+        public void Page_ShouldContainOptimalChargingWindowSection_WhenHomePageIsOpened()
+        {
+            // Arrange
+            using IWebDriver driver = new EdgeDriver();
+
+            // Act
+            driver.Navigate().GoToUrl("https://energy-mix-frontend-53hj.onrender.com");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+
+            // Assert
+            Assert.True(driver.FindElement(By.ClassName("optimal-window-section")).Displayed);
+        }
+
+        [Fact]
+        public void Page_ShouldContainFooter_WhenHomePageIsOpened()
+        {
+            // Arrange
+            using IWebDriver driver = new EdgeDriver();
+
+            // Act
+            driver.Navigate().GoToUrl("https://energy-mix-frontend-53hj.onrender.com");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+
+            // Assert
+            Assert.True(driver.FindElement(By.TagName("footer")).Displayed);
+        }
+
+        [Fact]
+        public void Page_ShouldContainThreeCharts_WhenHomePageIsOpened()
+        {
+            // Arrange
+            using IWebDriver driver = new EdgeDriver();
+
+            // Act
+            driver.Navigate().GoToUrl("https://energy-mix-frontend-53hj.onrender.com");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            var cards = driver.FindElements(By.ClassName("energy-card"));
+
+            // Assert
+            Assert.Equal(3, cards.Count);
+            Assert.All(cards, card => Assert.True(card.Displayed));
+        }
+
+        [Fact]
+        public void OptimalWindow_ShouldReturnOptimalWindowInfo_WhenUserSendRequest()
+        {
+            // Arrange
+            using IWebDriver driver = new EdgeDriver();
+
+            // Act
+            driver.Navigate().GoToUrl("https://energy-mix-frontend-53hj.onrender.com");
+            driver.FindElement(By.ClassName("find-window-button")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            // Assert
+            Assert.True(driver.FindElement(By.ClassName("optimal-window-result")).Displayed);
+        }
     }
 }
